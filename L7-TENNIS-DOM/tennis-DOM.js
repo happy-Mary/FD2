53f6e74808b4fe1a5c596af1e25ff41147d81555
happy-Mary <mariashklovska1991@gmail.com>
2016-11-25 22:37:38 +0300

    Game tennis started(DOM-js)

--- L7-TENNIS-DOM/tennis-DOM.js
+++ L7-TENNIS-DOM/tennis-DOM.js
@@ -1,43 +1,101 @@
-var header = document.getElementById("header");
-var box = document.getElementById("container");
+var counter = document.getElementById("counter");
 
+counter.style.width = "500px";
 //создание счета
-var counter = document.createElement("span");
-var left = 5;
-var right = 3;
+var countLeft = document.createElement("span");
+var countRight = document.createElement("span");
+var centerD = document.createElement("span");
+centerD.innerHTML=":";
+var left = 0;
+var right = 0;
+
 counter.style.fontSize = "4em";
 counter.style.display = "inline-block";
-counter.style.margin = "5px 200px";
-counter.innerHTML = left + ":" + right;
-header.appendChild(counter);
+countLeft.innerHTML = left;
+countRight.innerHTML = right;
+
+counter.appendChild(countLeft);
+counter.appendChild(centerD);
+counter.appendChild(countRight);
+
+//ХЭШ ПОЛЯ
+var AreaH = {
+	width: 600,
+	height: 400
+}  
+
+//ХЭШ РАКЕТКИ ЛЕВОЙ
+var LeftH = {
+	width: 15,
+	height: 100,
+	speedY: 1,
+	posY: 0,
+	posX: 0
+}
+
+//ХЭШ РАКЕТКИ ПРАВОЙ
+var RightH = LeftH;
+
 
 //создание поля
-box.style.cssText = "width: 600px; height: 400px; background-color: #FFFACD; border: 1px solid black; position: relative";
+var main_cont = document.getElementById("сontainer");
+main_cont.style.cssText = "width: 600px; text-align: center";
+var box = document.createElement("div");
+main_cont.appendChild(box);
 
-//элементы поля
+box.style.width = AreaH.width + "px";
+box.style.height = AreaH.height + "px";
+box.style.background = "orange";
+box.style.position = "relative";
+
+//ЭЛЕМЕНТЫ ПОЛЯ
+//левая ракетка
 var leftPlayer = document.createElement("div");
-leftPlayer.style.cssText = "width: 15px; height: 120px; background-color: #2E8B57; position: absolute";
-leftPlayer.style.top = "0";
-leftPlayer.style.left = "0";
+leftPlayer.style.backgroundColor = "blue";
+leftPlayer.style.position = "absolute";
+leftPlayer.style.width = LeftH.width + "px";
+leftPlayer.style.height = LeftH.height + "px";
+leftPlayer.style.top = LeftH.posY + "px";
+leftPlayer.style.left = LeftH.posX + "px";
 box.appendChild(leftPlayer);
 
-
+// правая ракетка
 var rightPlayer = document.createElement("div");
-rightPlayer.style.cssText = "width: 15px; height: 120px; background-color: #1E90FF; position: absolute";
-rightPlayer.style.top = "0";
-rightPlayer.style.right = "0";  // ??? left ????
+rightPlayer.style.backgroundColor = "green";
+rightPlayer.style.position = "absolute";
+rightPlayer.style.width = RightH.width + "px";
+rightPlayer.style.height =  RightH.height + "px";
+rightPlayer.style.top = RightH.posY + "px";
+rightPlayer.style.left = (AreaH.width - RightH.width) + "px";
 box.appendChild(rightPlayer);
 
+window.addEventListener("keydown", MovePl);
 
-var ball = document.createElement("div");
-ball.style.cssText = "width: 40px; height:40px; background-color: #B22222; border-radius: 50%;  position: absolute";
-box.appendChild(ball);
+function MovePl() {
+
+}
 
 
 
-//??? мяч в центре
-ball.style.top = (box.offsetHeight/2 - ball.offsetHeight/2)  + "px";
-ball.style.left = (box.offsetWidth/2 - ball.offsetWidth/2) + "px";
+
+
+// console.log(box.width);
+
+// rightPlayer.style.cssText = "width: 15px; height: 120px; background-color: ; position: absolute";
+// rightPlayer.style.top = "0";
+// rightPlayer.style.right = "0";  // ??? left ????
+// container.appendChild(rightPlayer);
+
+
+// var ball = document.createElement("div");
+// ball.style.cssText = "width: 40px; height:40px; background-color: #B22222; border-radius: 50%;  position: absolute";
+// container.appendChild(ball);
+
+
+
+// //??? мяч в центре
+// ball.style.top = (container.offsetHeight/2 - ball.offsetHeight/2)  + "px";
+// ball.style.left = (container.offsetWidth/2 - ball.offsetWidth/2) + "px";
 
 
 
